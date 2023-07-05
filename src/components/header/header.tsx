a4a9ea97c304ed1107506c954d715edc9ecf5a84
CONESTOGAC\ssande <ssande@conestogac.on.ca>
2023-07-05 11:38:36 -0400

    set up header with evenly spaced items

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,12 @@ import HeaderCSS from './header.module.css'
 function Header() {
     return (
       <header className={HeaderCSS.title}>
-        Header
+        <ul className={HeaderCSS.tileList}>
+            <li className={HeaderCSS.tile}>1</li>
+            <li className={HeaderCSS.tile}>2</li>
+            <li className={HeaderCSS.tile}>3</li>
+            <li className={HeaderCSS.tile}>4</li>
+        </ul>
       </header>
     )
   }
