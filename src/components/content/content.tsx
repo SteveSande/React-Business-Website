5aa22bbc1d00fb2cc65d08048dc4b2e68093009e
CONESTOGAC\ssande <ssande@conestogac.on.ca>
2023-07-05 10:38:20 -0400

    base header and content, experimenting with css modules

---
+++ src/components/content/content.tsx
@@ -0,0 +1,12 @@
+import ContentCSS from './content.module.css'
+
+function Content() {
+
+    return (
+      <div className='card'>
+        <p>Content</p>
+      </div>
+    )
+  }
+  
+  export default Content
---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import HeaderCSS from './header.module.css'
+
+function Header() {
+    return (
+      <header className="card">
+        <h1>Header</h1>
+      </header>
+    )
+  }
+  
+  export default Header
