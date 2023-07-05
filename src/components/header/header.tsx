865758597cedb04e9b9b2b600bc376b6e3c482e4
CONESTOGAC\ssande <ssande@conestogac.on.ca>
2023-07-05 13:25:55 -0400

    set up header for my use case

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,10 +4,8 @@ function Header() {
     return (
       <header className={HeaderCSS.title}>
         <ul className={HeaderCSS.tileList}>
-            <li className={HeaderCSS.tile}>1</li>
-            <li className={HeaderCSS.tile}>2</li>
-            <li className={HeaderCSS.tile}>3</li>
-            <li className={HeaderCSS.tile}>4</li>
+            <li className={HeaderCSS.tile}><img src='IntEcoLogo.svg' /></li>
+            <li className={HeaderCSS.tile}><h1>Interior Ecology Aquarium Services</h1></li>
         </ul>
       </header>
     )
