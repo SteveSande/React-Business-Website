1fd1902e806b230783a1e68398a6de92fa6323e6
CONESTOGAC\ssande <ssande@conestogac.on.ca>
2023-07-05 15:19:48 -0400

    made image clickable for attribution. added footer placeholder.

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,7 +2,16 @@ import FooterCSS from './footer.module.css'
 
 function Footer() {
     return (
-      <p>Footer</p>
+      <footer className={FooterCSS.title}>
+        <ul className={FooterCSS.tileList}>
+            <li className={FooterCSS.tile}>
+                <img src='IntEcoLogo.svg' />
+            </li>
+            <li className={FooterCSS.tile}>
+                <h1>Interior Ecology Aquarium Services</h1>
+            </li>
+        </ul>
+      </footer>
     )
   }
   
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -5,7 +5,9 @@ function Main() {
     return (
       
       <>
-        <img className={MainCSS.image} src='platygra.jpg' /><p>Content</p>
+        <a target="_blank" href="https://www.flickr.com/photos/50049718@N06/5690884254/in/photolist-9ETgC3-4EJoSh-9yzaGv-21EfF6a-8EEFW-egpWoB-7cUFV2-G3kaN-egpZnK-48GhQb-fiwJLR-ESdDgJ-AgQ6Ti-98My4S-6J2Vem-egpYR8-yU2gZL-egvCPu-BWfZb-9yz9Hz-e2BRQG-96H25h-6YsGsX-egvBP7-a3xFW5-z4ebtR-bw492S-jn46h2-7vCeJo-8V5Q9T-A17qst-bfVwYR-7vH66o-jhscTc-z4dUtp-jn5imj-qYhds4-o5vJt-4BRdDi-7FUb1w-6WHxNh-kwgNFM-8DaT6m-9iVe64-b1itpx-bfV7Yg-8r7XMK-jn53qC-dKwdxA-9yCaKJ">
+          <img className={MainCSS.image} src='platygra.jpg' />
+        </a>
       </>
     )
   }
