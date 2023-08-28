44cfa0b7f178628c8ef306852d42da5b1a46706d
SteveSande <stevenandrewsande@gmail.com>
2023-08-28 13:35:05 -0400

    set up different images for mobile

--- src/components/main/infoblock/infoblock.tsx
+++ src/components/main/infoblock/infoblock.tsx
@@ -2,6 +2,7 @@ import InfoBlockCSS from "./infoblock.module.css";
 
 interface info {
   background: string;
+  backgroundSmall: string;
   text: string;
   link: string;
   alt: string;
@@ -11,11 +12,14 @@ function InfoBlock(props: info) {
   return (
     <div className={InfoBlockCSS.infoBlock}>
       <a target="_blank" href={props.link}>
-        <img
-          className={InfoBlockCSS.background}
-          src={props.background}
-          alt={props.alt}
-        />
+        <picture>
+          <source srcSet={props.backgroundSmall} media="(max-width:700px)" />
+          <img
+            className={InfoBlockCSS.background}
+            src={props.background}
+            alt={props.alt}
+          />
+        </picture>
       </a>
       <h1 className={InfoBlockCSS.text}>{props.text}</h1>
     </div>
