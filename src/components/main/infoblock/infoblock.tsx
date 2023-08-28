db426c6524fb7af8557a280bbb8a81ae3b576389
SteveSande <stevenandrewsande@gmail.com>
2023-08-28 13:13:02 -0400

    added image alt text

--- src/components/main/infoblock/infoblock.tsx
+++ src/components/main/infoblock/infoblock.tsx
@@ -1,20 +1,25 @@
-import InfoBlockCSS from './infoblock.module.css'
+import InfoBlockCSS from "./infoblock.module.css";
 
 interface info {
-    background: string;
-    text: string;
-    link: string;
+  background: string;
+  text: string;
+  link: string;
+  alt: string;
 }
 
-function InfoBlock(props:info) {
-    return (
-        <div className={InfoBlockCSS.infoBlock}>
-            <a target="_blank" href={props.link}>
-                <img className={InfoBlockCSS.background} src={props.background} />
-			</a>  
-            <h1 className={InfoBlockCSS.text}>{props.text}</h1>
-        </div>
-    )
+function InfoBlock(props: info) {
+  return (
+    <div className={InfoBlockCSS.infoBlock}>
+      <a target="_blank" href={props.link}>
+        <img
+          className={InfoBlockCSS.background}
+          src={props.background}
+          alt={props.alt}
+        />
+      </a>
+      <h1 className={InfoBlockCSS.text}>{props.text}</h1>
+    </div>
+  );
 }
-  
-export default InfoBlock
+
+export default InfoBlock;
