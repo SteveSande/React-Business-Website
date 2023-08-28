c4f7e61da6407b9b587dd6c3cf1275ae5dfee9fc
SteveSande <stevenandrewsande@gmail.com>
2023-08-28 16:53:22 -0400

    accessible contact links

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -9,22 +9,26 @@ function Contact() {
           id="linkedin-link"
           target="_blank"
           href="https://www.linkedin.com/in/steven-sande-a4264752/"
+          aria-label="See the owner-operator's LinkedIn profile."
         >
           <img
             id="linkedin-logo"
             className={ContactCSS.socialIcon}
             src="LinkedInLogo.svg"
+            alt="the LinkedIn logo"
           />
         </a>
         <a
           id="instagram-link"
           target="_blank"
           href="https://www.instagram.com/interiorecology/"
+          aria-label="See Interior Ecology's Instagram profile."
         >
           <img
             id="instagram-logo"
             className={ContactCSS.socialIcon}
             src="InstaLogo.png"
+            alt="the Instagram logo"
           />
         </a>
       </div>
